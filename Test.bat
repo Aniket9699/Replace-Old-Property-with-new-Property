0782f5a2675da3017d11d28a2ca266b18baffc5d
Aniket9699 <89641646+Aniket9699@users.noreply.github.com>
2023-12-18 20:37:33 +0530

    Update Test.bat

--- Test.bat
+++ Test.bat
@@ -8,17 +8,18 @@ for /f "tokens=*" %%a in ('%SystemRoot%\System32\ipconfig.exe ^| %SystemRoot%\Sy
     if "!outputLine:IPv4 Address=!" neq "!outputLine!" (
         REM Extract the IPv4 address using string manipulation
         set "ipv4Address=!outputLine:*: =!"
-        echo !ipv4Address!
+        echo IPv4 Address: !ipv4Address!
+        
+        REM Your additional commands using the IPv4 address go here
+        set "IP=!ipv4Address!"
+        %SystemRoot%\System32\findstr.exe "!IP:~1!" Windows-Agent-Details.txt | for /f "tokens=2 delims==" %%b in ('more') do (
+            echo Groovy Home Path: %%b
+            copy Update.groovy %%b\bin
+            cd /d %%b\bin\
+            dir
+            groovy.bat "Update.groovy"
+        )
     )
-)
-
-
-%SystemRoot%\System32\findstr.exe "%IP:~1%" Windows-Agent-Details.txt | for /f "tokens=2 delims==" %%b in ('more') do (
-    echo Groovy Home Path: %%b
-    copy Update.groovy %%b\bin
-    cd /d %%b\bin\
-    dir
-    groovy.bat "Update.groovy"
 )
 
 endlocal
