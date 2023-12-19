d5dc34cc8b1f3df45827ff0f2a9d380cf49099cf
Aniket9699 <89641646+Aniket9699@users.noreply.github.com>
2023-12-19 17:29:27 +0530

    Update Test.bat

--- Test.bat
+++ Test.bat
@@ -1,25 +1,17 @@
 @echo off
-setlocal EnableDelayedExpansion
+setlocal enabledelayedexpansion
 
-REM Run ipconfig and filter the output for the specified Ethernet adapter
-for /f "tokens=*" %%a in ('%SystemRoot%\System32\ipconfig.exe ^| %SystemRoot%\System32\findstr.exe /C:"Ethernet adapter Lan" /C:"IPv4 Address"') do (
-    set "outputLine=%%a"
-    REM Extract and display only the IPv4 Address line
-    if "!outputLine:IPv4 Address=!" neq "!outputLine!" (
-        REM Extract the IPv4 address using string manipulation
-        set "ipv4Address=!outputLine:*: =!"
-        echo IPv4 Address: !ipv4Address!
-        
-        REM Your additional commands using the IPv4 address go here
-        set "IP=!ipv4Address!"
-        %SystemRoot%\System32\findstr.exe "!IP:~1!" Windows-Agent-Details.txt | for /f "tokens=2 delims==" %%b in ('more') do (
-            echo Groovy Home Path: %%b
-            copy Update.groovy %%b\bin
-            cd /d %%b\bin\
-            dir
-            groovy.bat "Update.groovy"
-        )
-    )
+for /f "tokens=*" %%a in ('hostname') do set myVariable=%%a
+echo Hostname: !myVariable!
+
+for /f "tokens=*" %%a in ('ping !myVariable! -4 ^| findstr /c:"Ping statistics for "') do (
+    echo %%a
+    set "lastValue="
+	for %%i in (%%a) do (
+    	set "lastValue=%%i"
+	)
+	set "IpAddress=!lastValue::=!"
+	echo Ip Address : !IpAddress!
 )
 
 endlocal
